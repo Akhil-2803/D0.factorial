1d8162ffd94c555867065e219260907f2621592f
Akhil-2803 <98790123+Akhil-2803@users.noreply.github.com>
2022-09-08 10:19:42 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int i,fact=1,number; 
+if ( 
+cout<<"Enter any Number:");    
+cin>>number;  
+for(i=1;i<=number;i++);
+{    
+fact=fact*i;    
+} 
+cout<<"Factorial of number" 
+<<number<<"is:"
+else("invalid input");
+<<fact<<endl;
+  }
